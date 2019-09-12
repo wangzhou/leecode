bfc3e5c998d4a8af0b4a2435aeea2ce41a7878dd
Zhou Wang <wangzhou1@hisilicon.com>
2019-09-12 13:53:46 +0800

    31 done
    Signed-off-by: Zhou Wang <wangzhou1@hisilicon.com>

--- 31/test.c
+++ 31/test.c
@@ -7,37 +7,47 @@ bool is_biggest(int* nums, int numsSize)
                        return false;
         return true;
 }
+
+void swap(int *a, int *b)
+{
+        int tmp;
         
+        tmp = *a;
+        *a = *b;
+        *b = tmp;
+}
+
+void revert(int *nums, int first, int last)
+{
+        int i;
+        
+        for (i = 0; i < (last - first + 1) / 2; i++)
+                swap(nums + first + i, nums + last - i);
+}
+
 void change_to_smallest(int* nums, int numsSize)
 {
-        int i, k;        
-        
-        for (i = 0; i < numsSize / 2; i++) {
-                k = nums[i];
-                nums[i] = nums[numsSize - i - 1];
-                nums[numsSize - i - 1] = k;
-        }
+        revert(nums, 0, numsSize - 1);
 }
 
 void nextPermutation(int* nums, int numsSize)
 {
-        int i, j, tmp;
+        int i, j;
         
-        if (is_biggest(nums, numsSize))
+        if (is_biggest(nums, numsSize)) {
                 change_to_smallest(nums, numsSize);
-        
-        if (numsSize == 2) {
-                tmp = nums[0];
-                nums[0] = nums[1];
-                nums[1] = tmp;
+                return;
         }
         
-        for (i = 1; i <= numsSize - 2; i++) {
+        if (numsSize == 2)
+                swap(nums, nums + 1);
+
+        for (i = 1; i <= numsSize - 1; i++) {
                 for (j = 0; j < i; j++) {
                        if (nums[numsSize - j - 1] > nums[numsSize - i - 1]) {
-                              tmp = nums[numsSize - j - 1];
-                              nums[numsSize - j - 1] = nums[numsSize - i - 1];
-                              nums[numsSize - i - 1] = tmp;
+                              swap(nums + numsSize - j - 1, nums + numsSize - i - 1);
+                              revert(nums, numsSize - i, numsSize - 1);
+                              return;
                        }
                 }
         }
