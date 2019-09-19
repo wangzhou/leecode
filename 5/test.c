e49d68d25c7a0607cd0a356d160794ff6fbd6616
Zhou Wang <wangzhou1@hisilicon.com>
2019-09-19 13:02:43 +0800

    ...
    Signed-off-by: Zhou Wang <wangzhou1@hisilicon.com>

--- 5/test.c
+++ 5/test.c
@@ -2,7 +2,7 @@
 char *find_pstr(char *mid, int i, int *length, int s_len, int flag)
 {
        int max, first, last, len;
-        
+
        if (flag == 0) {
                max = 1;
                first = i - 1;
@@ -10,7 +10,7 @@ char *find_pstr(char *mid, int i, int *length, int s_len, int flag)
                while (first >= 0 && last <= s_len - 1) {
                         if (*(mid - max) != *(mid + max))
                                 break;
-                        max++;       
+                        max++;
                         first--;
                         last++;
                }
@@ -22,7 +22,7 @@ char *find_pstr(char *mid, int i, int *length, int s_len, int flag)
                while (first >= 0 && last <= s_len - 1) {
                         if (*(mid - max) != *(mid + max + 1))
                                 break;
-                        max++;       
+                        max++;
                         first--;
                         last++;
                }
@@ -30,41 +30,41 @@ char *find_pstr(char *mid, int i, int *length, int s_len, int flag)
        } else {
                return NULL;
        }
-        
+
        *length = len;
-       return mid - max + 1; 
+       return mid - max + 1;
 }
 
 char *longestPalindrome(char *s)
 {
         int i, length, sub_len, max = 1;
         char *ret, *tmp, *curr;
-        
+
         if (!s || !(length = strlen(s)))
                 return s;
-        
+
         for (i = 0; i < length; i++) {
                 curr = find_pstr(s + i, i, &sub_len, length, 0);
                 if (sub_len >= max) {
-                       max = sub_len; 
+                       max = sub_len;
                        tmp = curr;
                 }
-                
+
                 if (i != length - 1) {
                         curr = find_pstr(s + i, i, &sub_len, length, 1);
                         if (sub_len >= max) {
-                                max = sub_len; 
+                                max = sub_len;
                                 tmp = curr;
                         }
                 }
         }
-        
+
         ret = (char *)malloc(max + 1);
         if (!ret)
                 return NULL;
-        
-        strncpy(ret, tmp, max); 
+
+        strncpy(ret, tmp, max);
         *(ret + max) = '\0';
-        
-        return ret; 
+
+        return ret;
 }
