bfa4e88ed3014201b992e67a60f6cfa4a3c2fa79
Zhou Wang <wangzhou1@hisilicon.com>
2019-09-22 23:07:06 +0800

    373 not finish
    Signed-off-by: Zhou Wang <wangzhou1@hisilicon.com>

---
+++ 373/test.c
@@ -0,0 +1,53 @@
+/**
+ * Return an array of arrays of size *returnSize.
+ * The sizes of the arrays are returned as *returnColumnSizes array.
+ * Note: Both returned array and *columnSizes array must be malloced, assume caller calls free().
+ */
+int **kSmallestPairs(int *nums1, int nums1Size, int *nums2, int nums2Size, int k, int *returnSize, int *returnColumnSizes)
+{
+        int **ret, i, j, n, curr_1, curr_2;
+        
+        ret = (int **)malloc(sizeof(int *) * k);  
+        if (!ret)
+                return NULL;
+        
+        for (i = 0; i < k; i++) {
+                *(ret + i) = (int *)malloc(sizeof(int) * 2);
+                if (!*(ret + i))
+                        goto err_free_array;
+        }
+        
+        returnColumnSizes = malloc(sizeof(int) * k);
+        if (!returnColumnSizes)
+               goto err_free_array;
+        
+        for (n = 0; n < k; n++)
+                *(returnColumnSizes + n) = 2;
+        
+        curr_1 = curr_2 = 0;
+        **ret = *nums1;
+        *(*ret + 1) = *nums2;              
+        
+        for (n = 1; n <= k - 1; n++) {
+                if (*(nums1 + curr_1) + *(nums2 + curr_2 + 1) <
+                    *(nums1 + curr_1 + 1) + *(nums2 + curr_2)) {
+                        **(ret + n) = *(nums1 + curr_1);
+                        *(*(ret + n) + 1) = *(nums2 + curr_2 + 1);              
+                        curr_2++;
+                } else {
+                        **(ret + n) = *(nums1 + curr_1 + 1);
+                        *(*(ret + n) + 1) = *(nums2 + curr_2);              
+                        curr_1++;
+                }             
+        }
+                
+        *returnSize = k;
+        return ret;
+        
+err_free_array:
+        for (j = 0; j < i; j++)
+                free(*(ret + j));
+err_free_ret:
+        free(ret);
+        return NULL;
+}
