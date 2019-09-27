8fe5d1c6c3b3b284f74aa53c01add3e88f27d3d0
Zhou Wang <wangzhou1@hisilicon.com>
2019-09-27 14:24:21 +0800

    ...
    Signed-off-by: Zhou Wang <wangzhou1@hisilicon.com>

--- 373/test.c
+++ 373/test.c
@@ -3,35 +3,36 @@
  * The sizes of the arrays are returned as *returnColumnSizes array.
  * Note: Both returned array and *columnSizes array must be malloced, assume caller calls free().
  */
-int **kSmallestPairs(int *nums1, int nums1Size, int *nums2, int nums2Size, int k, int *returnSize, int *returnColumnSizes)
+int **kSmallestPairs(int *nums1, int nums1Size, int *nums2, int nums2Size, int k, int *returnSize, int **returnColumnSizes)
 {
-        int **ret, i, j, n = 0, curr_1, curr_2;
+        int **ret, **ret_n, i, j, l, n = 0, curr_1, curr_2;
         
-        ret = (int **)malloc(sizeof(int *) * k);  
+        if (!nums1Size || !nums2Size) {
+                *returnSize = 0;
+                *returnColumnSizes = NULL;
+                return NULL;
+        }
+        
+        /* pre-allocate reture array */
+        ret = malloc(sizeof(int *) * k);  
         if (!ret)
                 return NULL;
         
         for (i = 0; i < k; i++) {
-                *(ret + i) = (int *)malloc(sizeof(int) * 2);
+                *(ret + i) = malloc(sizeof(int) * 2);
                 if (!*(ret + i))
                         goto err_free_array;
         }
         
-        returnColumnSizes = malloc(sizeof(int) * k);
-        if (!returnColumnSizes)
-               goto err_free_array;
-        
-        for (n = 0; n < k; n++)
-                *(returnColumnSizes + n) = 2;
-        
         curr_1 = curr_2 = 0;
         **ret = *nums1;
         *(*ret + 1) = *nums2;              
+        printf("%d: %d, %d\n", n, **(ret + n), *(*(ret + n) + 1));
         n++;
         
-        while ((curr_1 < nums1Size) && (curr_2 < nums2Size)) {
-                if (*(nums1 + curr_1) + *(nums2 + curr_2 + 1) <
-                    *(nums1 + curr_1 + 1) + *(nums2 + curr_2)) {
+        while (n < k && (curr_1 < nums1Size) && (curr_2 < nums2Size)) {
+                if ((*(nums1 + curr_1) + *(nums2 + curr_2 + 1)) <=
+                    (*(nums1 + curr_1 + 1) + *(nums2 + curr_2))) {
                         **(ret + n) = *(nums1 + curr_1);
                         *(*(ret + n) + 1) = *(nums2 + curr_2 + 1);              
                         curr_2++;
@@ -40,11 +41,40 @@ int **kSmallestPairs(int *nums1, int nums1Size, int *nums2, int nums2Size, int k
                         *(*(ret + n) + 1) = *(nums2 + curr_2);              
                         curr_1++;
                 }             
+                printf("%d: %d, %d\n", n, **(ret + n), *(*(ret + n) + 1));
                 n++;
         }
+        
+        *returnColumnSizes = malloc(sizeof(int) * n);
+        if (!*returnColumnSizes)
+                goto err_free_array;
+        
+        for (l = 0; l < n; l++)
+                *(*returnColumnSizes + l) = 2;
                 
         *returnSize = n;
-        return ret;
+        
+        if (n == k) {
+                return ret;
+        } else {
+                ret_n = malloc(sizeof(int *) * n);  
+                if (!ret_n)
+                        return NULL;
+        
+                for (i = 0; i < n; i++) {
+                        *(ret_n + i) = malloc(sizeof(int) * 2);
+                        if (!*(ret_n + i))
+                                goto err_free_array;
+                        **(ret_n + i) = **(ret + i);
+                        *(*(ret_n + i) + 1) = *(*(ret + i) + 1);
+                }               
+                
+                for (j = 0; j < k; j++)
+                        free(*(ret + j));
+                free(ret);
+                
+                return ret_n;
+        }
         
 err_free_array:
         for (j = 0; j < i; j++)
